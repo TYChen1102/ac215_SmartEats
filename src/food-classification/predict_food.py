2bdaede3fe6afc958740d2ae9cf85a727d99b6e3
TYChen1102 <chentaiyang1102@gmail.com>
2024-10-14 22:06:28 -0400

    Update step1 and step3

--- src/food-classification/predict_food.py
+++ src/food-classification/predict_food.py
@@ -1,47 +1,29 @@
-import pandas as pd
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import cv2
 
-model = load_model('food_model_EfficientNet.h5')
+#import os
+#import shutil
+#import glob
+from google.cloud import storage
+#import argparse
+import json
 
-num_to_food = {
-    0:'Baked Potato',
-    1:'Crispy Chicken',
-    2:'Donut',
-    3:'Fries',
-    4:'Hot Dog',
-    5:'Sandwich',
-    6:'Taco', 
-    7:'Taquito', 
-    8:'apple_pie', 
-    9:'burger', 
-    10:'butter_naan', 
-    11:'chai', 
-    12:'chapati', 
-    13:'cheesecake', 
-    14:'chicken_curry', 
-    15:'chole_bhature', 
-    16:'dal_makhani', 
-    17:'dhokla', 
-    18:'fried_rice', 
-    19:'ice_cream', 
-    20:'idli', 
-    21:'jalebi', 
-    22:'kaathi_rolls', 
-    23:'kadai_paneer', 
-    24:'kulfi', 
-    25:'masala_dosa', 
-    26:'momos', 
-    27:'omelette', 
-    28:'paani_puri', 
-    29:'pakode', 
-    30:'pav_bhaji', 
-    31:'pizza', 
-    32:'samosa', 
-    33:'sushi'
-}
+gcp_project = "ac215-project"
+bucket_name = "ac215smarteat"
+
+# Download test image
+def download():
+
+    print("download")
+
+    storage_client = storage.Client(project=gcp_project)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob('shared_results/test_food.png')
+    blob.download_to_filename('test_food.png')
+
+    print('Step1 input downloaded from GCP bucket.')
 
 # Read and process the image
 def process_image(img_path):
@@ -51,25 +33,73 @@ def process_image(img_path):
     return img
 
 # Predict on the image
-def make_prediction(img_path):
-    img = process_image(img_path)
+def make_prediction(img):
+    num_to_food = {
+        0:'Baked Potato',
+        1:'Crispy Chicken',
+        2:'Donut',
+        3:'Fries',
+        4:'Hot Dog',
+        5:'Sandwich',
+        6:'Taco', 
+        7:'Taquito', 
+        8:'apple_pie', 
+        9:'burger', 
+        10:'butter_naan', 
+        11:'chai', 
+        12:'chapati', 
+        13:'cheesecake', 
+        14:'chicken_curry', 
+        15:'chole_bhature', 
+        16:'dal_makhani', 
+        17:'dhokla', 
+        18:'fried_rice', 
+        19:'ice_cream', 
+        20:'idli', 
+        21:'jalebi', 
+        22:'kaathi_rolls', 
+        23:'kadai_paneer', 
+        24:'kulfi', 
+        25:'masala_dosa', 
+        26:'momos', 
+        27:'omelette', 
+        28:'paani_puri', 
+        29:'pakode', 
+        30:'pav_bhaji', 
+        31:'pizza', 
+        32:'samosa', 
+        33:'sushi'
+    }
     predictions = model.predict(img)[0]
     pred_class = np.argmax(predictions)
     pred_class = num_to_food[pred_class]
     return pred_class, predictions
 
-img_path = 'test_food.png'
-label, probabilities = make_prediction(img_path)
+# Save preditions and upload output to bucket shared_results folder
+def send_output(label, probabilities):
+
+    output = {
+        'food': label,
+        'weight': probabilities.tolist()
+    }
+
+    with open('step1_output.json', 'w') as outfile:
+        json.dump(output, outfile)
+    
+    print("upload")
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob('shared_results/step1_output.json')
+    blob.upload_from_filename('step1_output.json')
+
+    print('Step1 output uploaded to GCP bucket.')
+
+model = load_model('food_model_EfficientNet.h5')
+download()
+img = process_image('test_food.png')
+label, probabilities = make_prediction(img)
 print("Predicted food:")
 print(label)
 print("Predicted probability:")
 print(probabilities)
-# Save preditions
-df = pd.DataFrame({
-    'Predicted': [label]
-})
-
-for class_index, class_name in num_to_food.items():
-  df[f'Prob_{class_name}'] = probabilities[class_index]
-
-df.to_csv('predictions_test.csv', index=True)
+send_output(label, probabilities)
---
+++ src/nutrition_predict_disease/nutrition_predict_disease.py
@@ -0,0 +1,13 @@
+# Import necessary libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the nutrition information
+
+
+# Use machine learning models to predict disease risk
+
+
+# Output risks
