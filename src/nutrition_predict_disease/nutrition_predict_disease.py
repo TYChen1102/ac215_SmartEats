2dd76b7ca0a3ce79e667ee7ce38088a0a02c3316
Jiayi Sun <jsunsun0805@gmail.com>
2024-10-17 23:41:02 -0400

    update part3

--- src/nutrition_predict_disease/nutrition_predict_disease.py
+++ src/nutrition_predict_disease/nutrition_predict_disease.py
@@ -28,36 +28,55 @@ def extract_input():
     print(step2_output)
     return step2_output
 
-# Load trained models
-obesity_model = joblib.load('/models/Obesity_model.pkl')
-diabetes_model = joblib.load('/models/Diabetes_model.pkl')
-high_cholesterol_model = joblib.load('/models/High Cholesterol_model.pkl')
-hypertension_model = joblib.load('/models/Hypertension_model.pkl')
+def transform_data(json_data):
+    nutritional_columns = ['Carbohydrate (G)', 'Energy (KCAL)', 'Protein (G)', 'Fat (G)']
+    transformed_data = {
+        'Carbohydrate (G)': float(json_data['Carbohydrate'].split()[0]),  # Extract number
+        'Energy (KCAL)': float(json_data['Energy'].split()[0]),            # Extract number
+        'Protein (G)': float(json_data['Protein'].split()[0]),             # Extract number
+        'Fat (G)': float(json_data['Fat'].split()[0])                      # Extract number
+    }
+    df_transformed = pd.DataFrame([transformed_data])
+    print(df_transformed)
+    return df_transformed
 
-# Output risks
+def send_output(output):
+    # Write the result to a shared folder
+    with open('step3_output.json', 'w') as outfile:
+        json.dump(output, outfile)
 
+    # Upload output to bucket shared_results folder
+    print("upload")
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob('shared_results/step3_output.json')
+    blob.upload_from_filename('step3_output.json')
 
-# # Save preditions and upload output to bucket shared_results folder
-# def send_output(label, probabilities):
-
-#     output = {
-#         'food': label,
-#         'weight': probabilities.tolist()
-#     }
-
-#     with open('step3_output.json', 'w') as outfile:
-#         json.dump(output, outfile)
-    
-#     print("upload")
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob('shared_results/step3_output.json')
-#     blob.upload_from_filename('step3_output.json')
-
-#     print('Step3 output uploaded to GCP bucket.')
-
-
+    print('Step3 output uploaded to GCP bucket.')
 
 ## Run the above functions
 step2_output = extract_input()
-step2_output
+step2_output_df = transform_data(step2_output)
+
+# # Use machine learning models to predict disease risk
+# # Load trained models
+obesity_model = joblib.load('models/Obesity_model.pkl')
+diabetes_model = joblib.load('models/Diabetes_model.pkl')
+high_cholesterol_model = joblib.load('models/High Cholesterol_model.pkl')
+hypertension_model = joblib.load('models/Hypertension_model.pkl')
+
+# Predict risks
+# Predict the probability of each disease
+obesity_prob = obesity_model.predict_proba(step2_output_df)[0][1]  # Probability of having obesity
+diabetes_prob = diabetes_model.predict_proba(step2_output_df)[0][1]  # Probability of having diabetes
+high_cholesterol_prob = high_cholesterol_model.predict_proba(step2_output_df)[0][1]  # Probability of high cholesterol
+hypertension_prob = hypertension_model.predict_proba(step2_output_df)[0][1]  # Probability of having hypertension
+
+output = {
+    'Obesity': round(obesity_prob, 4).astype(str),
+    'Diabetes': round(diabetes_prob, 4).astype(str),
+    'High Cholesterol': round(high_cholesterol_prob, 4).astype(str),
+    'Hypertension': round(hypertension_prob, 4).astype(str)
+}
+
+send_output(output)
