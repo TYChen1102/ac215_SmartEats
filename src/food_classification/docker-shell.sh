2716930788c35535f1ecf93247ee8a2a2a80fc5f
Jiayi Sun <63036147+jsunsun0805@users.noreply.github.com>
2024-11-18 00:40:41 -0500

    Update docker-shell.sh

--- src/food_classification/docker-shell.sh
+++ src/food_classification/docker-shell.sh
@@ -8,16 +8,17 @@ export SECRETS_DIR=$(pwd)/secrets/
 export GCP_PROJECT="ac215-smarteat-437821"
 export GCS_MODELS_BUCKET_NAME="ac215smarteat"
 
-# Build the image based on the Dockerfile
-#docker build -t $IMAGE_NAME -f Dockerfile .
-# M1/2 chip macs use this line
+# Build the image
 docker build -t $IMAGE_NAME -f Dockerfile .
+
+# Debugging: Verify IMAGE_NAME
+echo "Using image: $IMAGE_NAME"
 
 # Run Container
 docker run --rm --name $IMAGE_NAME -ti \
--v "$BASE_DIR":/app \
--v "$SECRETS_DIR":/secrets \
--e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ac215-smarteat-437821-15c2f229e610.json
--e GCP_PROJECT=$GCP_PROJECT \
--e GCS_MODELS_BUCKET_NAME=$GCS_MODELS_BUCKET_NAME \
-$IMAGE_NAME
+  -v "$BASE_DIR":/app \
+  -v "$SECRETS_DIR":/secrets \
+  -e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ac215-smarteat-437821-15c2f229e610.json \
+  -e GCP_PROJECT=$GCP_PROJECT \
+  -e GCS_MODELS_BUCKET_NAME=$GCS_MODELS_BUCKET_NAME \
+  $IMAGE_NAME
