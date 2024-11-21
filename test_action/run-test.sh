a3bb6f7e828798228a795fe9dd419bae7850976c
Yantong Cui <143852554+yanc0828@users.noreply.github.com>
2024-11-20 21:23:00 -0500

    Update run-test.sh

--- test_action/run-test.sh
+++ test_action/run-test.sh
@@ -5,4 +5,6 @@ pip install pytest-cov
 pytest --version
 
 #pytest --cov=. --cov-report=html:./htmlcov tests.py
+mkdir -p /test_action/htmlcov
+chmod 777 /test_action/htmlcov
 pytest --cov=/test_action/htmlcov --cov-report=html tests.py
