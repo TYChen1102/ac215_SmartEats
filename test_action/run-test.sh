729e3078350c30211dcf307e4df2c65962cb9d60
Jiayi Sun <jsunsun0805@gmail.com>
2024-12-10 20:01:18 -0500

    add pytest.ini

--- test_action/run-test.sh
+++ test_action/run-test.sh
@@ -7,4 +7,4 @@ pytest --version
 #pytest --cov=. --cov-report=html:./htmlcov tests.py
 mkdir -p /test_action/htmlcov
 chmod 777 /test_action/htmlcov
-pytest --cov=. --cov-report=html:/test_action/htmlcov tests.py test_chat_utils.py test_llm_rag_chat.py test_llm_rag_utils.py test_service.py
+pytest --cov=. --cov-report=html:/test_action/htmlcov tests.py test_chat_utils.py test_llm_rag_chat.py test_llm_rag_utils.py
