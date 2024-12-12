58381c4307e0d1137a9ba2a1842f0f8e038dd1b3
TYChen1102 <chentaiyang1102@gmail.com>
2024-12-11 23:04:09 -0500

    Update deploy-k8s-update.sh

--- src_1container/deployment/deploy-k8s-update.sh
+++ src_1container/deployment/deploy-k8s-update.sh
@@ -1,2 +1,6 @@
 ansible-playbook deploy-docker-images.yml -i inventory.yml
+ansible-playbook deploy-create-instance.yml -i inventory.yml --extra-vars cluster_state=present
+ansible-playbook deploy-provision-instance.yml -i inventory.yml
+ansible-playbook deploy-setup-containers.yml -i inventory.yml
+ansible-playbook deploy-setup-webserver.yml -i inventory.yml
 ansible-playbook update-k8s-cluster.yml -i inventory-prod.yml
